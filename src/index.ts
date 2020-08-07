f18e9f3b9f20a6437363fa7116401193769e376f
Monkey <fanghuawfh@gmail.com>
2020-08-07 22:35:35 +0800

    自定义菜单

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import { 
   app, 
   BrowserWindow,
-  Menu
+  Menu,
+  MenuItem
  } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
+const template: any[] = [
+  {
+    label: '编辑',
+    submenu: [
+      {
+        label: '加载',
+        role: 'reload'
+      }
+    ]
+  },
+  {
+    label: '关于',
+    submenu: [
+      {
+        label: '版本'
+      }
+    ]
+  }
+]
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
